8c509140d00ede6afc471276bcf71929f39c9997
ninthcircle <mainlakshayhoon@gmail.com>
2026-02-19 23:49:26 +0530

    Add test files to visualize locking.

---
+++ src/test/java/fun/ninth/Process2.java
@@ -0,0 +1,27 @@
+package fun.ninth;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Process2 {
+    private static final String PURPOSE = "lock_test";
+
+    public static void main(String[] args) {
+        try (Connection connection = DbConnection.getConnection()) {
+
+            TransactionLock transactionLock = new TransactionLock(connection, PURPOSE);
+            try {
+                transactionLock.acquire();
+                // Add breakpoint below.
+                System.out.println("Lock acquired.");
+                transactionLock.release();
+                System.out.println("Lock released.");
+            } catch (Exception e) {
+                transactionLock.rollback();
+                throw e;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
